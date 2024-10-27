96da679529c4dd2a4c51e874cca281dbedd919e6
Fatim-coding <timabadjie12@gmail.com>
2024-10-27 15:26:40 +0000

    second commit

---
+++ main.py
@@ -0,0 +1,27 @@
+a = 16
+b = 15
+c = 13
+
+print(a != b)
+print(b != c)
+
+
+a = "python"
+b = " coding"
+
+if a != b :
+     print(a, 'and', b, 'are different.')
+
+
+a = 4
+b = 5
+
+if (a == 1) !=(b == 5):
+         print ('hello')
+
+
+
+
+         a = int(input("enter a number"))
+  if a%2 != 0 :
+            print(a, "is not even number")
